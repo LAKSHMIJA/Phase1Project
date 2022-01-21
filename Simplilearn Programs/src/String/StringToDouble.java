839020d6cd2bf548381b51ebd9bf2803e5fecf30
User <User@DESKTOP-TBRNDMS>
2022-01-21 08:39:02 +0530

    pROGRAMS

---
+++ Simplilearn Programs/src/String/StringToDouble.java
@@ -0,0 +1,17 @@
+package String;
+
+public class StringToDouble {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String str = "98536.756";
+
+		double str1 = Double.parseDouble(str);
+
+		System.out.println(str1);
+	}
+
+	}
+
+
---
+++ Simplilearn Programs/src/String/StringToint.java
@@ -0,0 +1,14 @@
+package String;
+
+public class StringToint {
+
+	public static void main(String[] args) {
+		 String str7 = "5";  
+	        Integer result2 = Integer.valueOf(str7); 
+	        System.out.println(result2);  
+		
+		}
+
+
+
+}
